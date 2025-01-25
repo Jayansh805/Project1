5bb6a29f71430bb4467fa9372040c6b9289ed1f9
Jayansh805 <jdgupta1980@gmail.com>
2025-01-25 15:06:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,56 @@ function change(page){
                 document.getElementById(`${pages[i]}`).style.display="flex";
             }
             else{
-                document.getElementById(`${pages[i]}`).style.display="block";
+                document.getElementById(`${pages[i]}`).style.display="flex";
             }
         }
     }
 }
+
+function cross_click() {
+    document.getElementById(`cross_in_nav`).classList.toggle("change");
+    document.getElementById(`cross_in_overlay`).classList.toggle("change");
+}
+
+function openNav() {
+    document.getElementById("myOverlay").style.height = "100%";
+  }
+  
+function closeNav() {
+    document.getElementById("myOverlay").style.height = "0%";
+  }
+
+function instagram(){
+    window.open('https://www.instagram.com/jayanshkhandelwal/');
+}
+
+function linkedin(){
+    window.open('https://www.linkedin.com/in/jayansh-khandelwal-5b94b4326/');
+}
+
+
+const options = ["opt1","opt2","opt3","opt4"];
+
+function change_color(x){
+    for(let i=0; i<options.length; i++){
+        document.getElementById(`${options[i]}`).style.color = "black";
+    }
+    document.getElementById(`${x}`).style.color="blue";
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    let mysBtn = document.getElementById('scrollbtn');
+
+    if (mysBtn) { //Check if the element exists!
+        window.addEventListener('scroll', function () {
+            if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+                mysBtn.style.display = 'block';
+            } else {
+                mysBtn.style.display = 'none';
+            }
+        });
+    } else {
+        console.error("Element with ID 'scrollbtn' not found.");
+    }
+});
